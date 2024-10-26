118e58ddf27c685b19dc16bf53dce34718cd8517
efettf <efettf@proton.me>
2024-10-26 22:10:59 +0200

    rename vim options to keys

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -2,7 +2,7 @@
   imports = [
     {programs.nixvim = lib.importTOML ./options.toml;}
     {programs.nixvim.plugins = lib.importTOML ./plugins.toml;}
-    {programs.nixvim.extraConfigVim = builtins.readFile ./options.vim;}
+    {programs.nixvim.extraConfigVim = builtins.readFile ./keys.vim;}
   ];
   programs.nixvim.enable = true;
 }
