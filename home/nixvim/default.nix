9c4cecd8218fe7fd432a999d5ab67c0184d354c7
efettf <efettf@proton.me>
2024-10-26 22:40:27 +0200

    :truck: Normalize toml config file names

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -1,6 +1,6 @@
 {lib, ...}: {
   imports = [
-    {programs.nixvim = lib.importTOML ./options.toml;}
+    {programs.nixvim = lib.importTOML ./config.toml;}
     {programs.nixvim.plugins = lib.importTOML ./plugins.toml;}
     {programs.nixvim.extraConfigVim = builtins.readFile ./keys.vim;}
   ];
