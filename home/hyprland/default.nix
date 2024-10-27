c775518420daae95f236ed98edae08bc050cacef
efettf <efettf@proton.me>
2024-10-27 22:00:59 +0100

    improve looks of hyprland and vim

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,7 +1,10 @@
-{lib, ...}: {
+{lib, config, ...}: {
   imports = [
     {wayland.windowManager.hyprland.settings = lib.importTOML ./config.toml;}
   ];
+
+  wayland.windowManager.hyprland.settings.general."col.inactive_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base01})";
+  wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base04})";
 
   # Enable hyprland.
   wayland.windowManager.hyprland.enable = true;
