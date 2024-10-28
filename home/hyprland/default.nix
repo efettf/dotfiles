30d60c4382c65b258dc9c124a476f1313ea09b60
efettf <efettf@proton.me>
2024-10-28 17:52:50 +0100

    improve nix code structure in hyprland config

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -3,8 +3,10 @@
     {wayland.windowManager.hyprland.settings = lib.importTOML ./config.toml;}
   ];
 
-  wayland.windowManager.hyprland.settings.general."col.inactive_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base01})";
-  wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base04})";
+  wayland.windowManager.hyprland.settings.general = {
+    "col.inactive_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base02})";
+    "col.active_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base04})";
+  };
 
   # Enable hyprland.
   wayland.windowManager.hyprland.enable = true;
