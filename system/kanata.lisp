5bc1f3ed85ce043a82ce20f963b179772e50dc6f
efettf <efettf@proton.me>
2024-11-01 17:40:48 +0100

    remove home row mods

--- system/kanata.lisp
+++ system/kanata.lisp
@@ -3,31 +3,29 @@
   esc 
   tab 
   caps
+  spc
   ralt
-  f
   h j k l 
   n g
   e d
 )
 
 (defalias
-  tab-hm (tap-hold 75 150 tab (layer-while-held nav))
-  esc-hm (tap-hold 75 150 esc lctl) 
+  tab-th (tap-hold 75 150 tab (layer-while-held nav))
+  esc-th (tap-hold 75 150 esc lctl) 
+  spc-th (tap-hold 75 150 spc lsft)
   alt-os (one-shot 3000 lmet)
-  f-hm (tap-hold 75 150 f (layer-while-held nav))
-  e-hm (tap-hold 75 150 e lsft)
-  j-hm (tap-hold 75 150 j lsft)
 )
 
 (deflayer base
   caps 
-  @tab-hm
-  @esc-hm
+  @tab-th
+  @esc-th
+  @spc-th
   @alt-os
-  @f-hm
-  _ @j-hm _ _ 
+  _ _ _ _ 
   g n
-  d @e-hm
+  d e
 )
 
 (deflayer nav
