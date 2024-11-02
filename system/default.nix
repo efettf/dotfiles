d1e1a61cea47cbfffac403e5871a0e1a56d8f260
efettf <efettf@proton.me>
2024-11-02 22:14:28 +0100

    remove obvious source code comment

--- system/default.nix
+++ system/default.nix
@@ -9,7 +9,6 @@
     {services.kanata.keyboards.main.config = builtins.readFile ./kanata.lisp;}
   ];
 
-  # Replace sudo with doas completly.
   environment.systemPackages = [(pkgs.writeScriptBin "sudo" ''exec doas "$@"'')];
 
   # Set default user shell to zsh.
