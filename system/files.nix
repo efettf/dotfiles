2a0d645e0f2054c90bcf22e39a3f7817726b9e4d
efettf <efettf@proton.me>
2024-11-02 21:24:17 +0100

    upgrade homix

--- system/files.nix
+++ system/files.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   homix = {
-    # "wallpapers".source = pkgs.fetchgit {
-    #   url = "https://github.com/efettf/wallpapers";
-    #   hash = "sha256-cy1st7tYlkkPfrvcg9Eo61q+gsrfr0OZcHyIrZ6RioA=";
-    # };
+    "wallpapers".source = pkgs.fetchgit {
+      url = "https://github.com/efettf/wallpapers";
+      hash = "sha256-cy1st7tYlkkPfrvcg9Eo61q+gsrfr0OZcHyIrZ6RioA=";
+    };
     ".config/ohmyposh/config.toml".source = ../home/programs/oh-my-posh.toml;
   };
 }
