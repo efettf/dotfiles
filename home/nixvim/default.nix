707b5ef7383567edce21bd52671ae7d0518365c6
efettf <efettf@proton.me>
2024-11-02 09:17:53 +0100

    add cmp emoji in vim

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -1,8 +1,26 @@
-{lib, ...}: {
+{
+  lib, 
+  pkgs, 
+  ... 
+}: let
+  cmp-emoji = pkgs.vimUtils.buildVimPlugin {
+    pname = "cmp-emoji";
+    version = "1.0";
+    src = pkgs.fetchgit {
+      url = "https://github.com/efettf/cmp-emoji.nvim";
+      hash = "sha256-AESd8x6IdlVeGMIzjbbfG0aYHvIaQTGkxa2Xh8lhC5Y=";
+    };
+  };
+in {
   imports = [
     {programs.nixvim = lib.importTOML ./config.toml;}
     {programs.nixvim.plugins = lib.importTOML ./plugins.toml;}
     {programs.nixvim.extraConfigVim = builtins.readFile ./extra.vim;}
   ];
-  programs.nixvim.enable = true;
+  programs.nixvim = {
+    enable = true;
+    extraPlugins = [
+      cmp-emoji
+    ];
+  };
 }
