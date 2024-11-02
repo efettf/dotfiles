f32d02f5305356c9b05f2a07778b1eea4ed5a673
efettf <efettf@proton.me>
2024-11-02 13:04:37 +0100

    remove unused kanata keys

--- system/kanata.lisp
+++ system/kanata.lisp
@@ -3,8 +3,6 @@
   esc 
   tab 
   caps
-  spc
-  ralt
   h j k l 
   n g
   e d
@@ -13,24 +11,18 @@
 (defalias
   tab-th (tap-hold 75 150 tab (layer-while-held nav))
   esc-th (tap-hold 75 150 esc lctl) 
-  spc-th (tap-hold 75 150 spc lsft)
-  alt-os (one-shot 3000 lmet)
 )
 
 (deflayer base
   caps 
   @tab-th
   @esc-th
-  @spc-th
-  @alt-os
   _ _ _ _ 
   g n
   d e
 )
 
 (deflayer nav
-  _
-  _
   _
   _
   _
