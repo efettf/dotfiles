f12a8b9d4a76c590c02364d3a18335d6300bb115
efettf <efettf@proton.me>
2024-11-03 13:36:37 +0100

    add nvim configuration

--- files.nix
+++ files.nix
@@ -4,7 +4,8 @@
       url = "https://github.com/efettf/wallpapers";
       hash = "sha256-Rinz1qPIaljSt0/dGb+kZEFGQXcQ855fI4EjZP1m7nM=";
     };
-    # ".config/nvim".source = ./neovim;
+    ".config/nvim/init.lua".source = ./nvim/init.lua;
+    ".config/nvim/lua/plugins".source = ./nvim/plugins;
     ".config/ohmyposh/config.toml".source = ./home/programs/oh-my-posh.toml;
   };
 }
--- nvim/init.lua
+++ nvim/init.lua
@@ -17,10 +17,19 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+vim.g.neovide_transparency = 0.85
+vim.g.neovide_padding_bottom = 10
+vim.g.neovide_padding_right = 7
+vim.g.neovide_padding_left = 1
+vim.g.neovide_padding_top = 10
 
--- require("lazy").setup({
---   spec.import = "plugins"
--- })
+require("lazy").setup({
+  spec = { import = "plugins" }
+})
+
+vim.api.nvim_set_keymap("n", "<c-=>", "<cmd>lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", "<c-->", "<cmd>lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<cr>", { silent = true })
 
 vim.o.autoread = true
 vim.o.backup = false
@@ -45,4 +54,5 @@ vim.o.swapfile = false
 vim.o.tabstop = 2
 vim.o.termguicolors = true
 vim.o.writebackup = false
+
 
