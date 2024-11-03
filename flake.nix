221d19c348c49e4af2ff33c9d4e4655988a799ce
efettf <efettf@proton.me>
2024-11-03 15:38:40 +0100

    make flake minimal

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
   outputs = inputs: {nixosConfigurations = import ./hosts inputs;};
 
-  inputs = {
-    # 'pkgs' source, you can change system version here.
-    nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-    homix.url = "github:efettf/homix.nix";
-
-    # Make sure to specify all follows here,
-    # to make sure flake won't have to download 2 nixpkgs.
-    homix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.homix = {
+    url = "github:efettf/homix.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 }
