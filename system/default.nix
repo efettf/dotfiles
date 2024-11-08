3c38c77b7313068f515a262eea585670d61591e4
efettf <efettf@proton.me>
2024-11-08 14:12:49 +0100

    add st

--- system/default.nix
+++ system/default.nix
@@ -11,6 +11,17 @@
 
   environment.systemPackages = with pkgs; [
     (inputs.wrapper-manager.lib.build { inherit pkgs; modules = [ ../wrappers.nix ]; })
+    (st.overrideAttrs (oldAttrs: rec {
+      buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
+      patches = [
+        (fetchpatch {
+          url = "https://st.suckless.org/patches/ligatures/0.9.2/st-ligatures-20240427-0.9.2.diff";
+          hash = "sha256-kFmGCrsqiphY1uiRCX/Gz4yOdlLxIIHBlsM1pvW5TTA=";
+        })
+      ];
+      configFile = writeText "config.def.h" (builtins.readFile ../programs/st.h);
+      postPatch = "${oldAttrs.postPatch}\n cp ${configFile} config.def.h";
+    }))
     gh
     fd
     git
