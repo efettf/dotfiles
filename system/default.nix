87ddbc15758824eaaf5c4bf6e9dd05d3c6ad8988
efettf <efettf@proton.me>
2024-11-09 11:29:29 +0100

    add swhkd

--- system/default.nix
+++ system/default.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: let
-  dwl = (pkgs.dwl.overrideAttrs (old: rec {
+  dwl = pkgs.dwl.overrideAttrs (old: rec {
     patches = [
       (pkgs.fetchpatch {
         url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/gaps/gaps.patch";
@@ -22,8 +22,8 @@
     configFile = pkgs.writeText "config.def.h" (builtins.readFile "${inputs.suckless}/dwl.h");
     postPatch = old.postPatch + "cp ${configFile} config.def.h";
     passthru.providedSessions = ["dwl"];
-  }));
-  st = (pkgs.st.overrideAttrs (old: rec {
+  });
+  st = pkgs.st.overrideAttrs (old: rec {
     buildInputs = old.buildInputs ++ [pkgs.harfbuzz];
     patches = [
       (pkgs.fetchpatch {
@@ -37,44 +37,95 @@
     ];
     configFile = pkgs.writeText "config.def.h" (builtins.readFile "${inputs.suckless}/st.h");
     postPatch = old.postPatch + "cp ${configFile} config.def.h";
-  }));
+  });
+  swhkd = pkgs.rustPlatform.buildRustPackage {
+    pname = "swhkd";
+    version = "1.2.1-unstable-2024-04-06";
+
+    outputs = ["bin" "man" "out"];
+
+    src = pkgs.fetchFromGitHub {
+      owner = "waycrate";
+      repo = "swhkd";
+      rev = "f8519a54900d72492a6c036b32e472c108d44dbf";
+      hash = "sha256-zyGyZOG8gAtsRkzSRH1M777fPv1wudbVsBrSTJ5CBnY=";
+    };
+
+    nativeBuildInputs = with pkgs; [
+      scdoc
+      pkg-config
+    ];
+
+    postPatch = ''
+      sed -ie 's/-o root//' Makefile
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+      make build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out $bin $man/share
+      make DESTDIR=$out MAN1_DIR=/share/man/man1 MAN5_DIR=/share/man/man5 TARGET_DIR=/bin install
+      mv $out/bin $bin/bin
+      mv $out/share/man $man/share/man
+      mv $out/usr/share/polkit-1 $out/share/polkit-1
+      rm -r $out/etc $out/usr
+
+      runHook postInstall
+    '';
+
+    cargoHash = "sha256-d/61hdyooYuqfOSTUcxVUJVhG98uexgPk7h6N1ptIgQ=";
+
+    buildInputs = with pkgs; [
+      systemd
+      libgcc
+    ];
+  };
 in {
   imports = [
     (lib.importTOML ./config.toml)
     {services.kanata.keyboards.main.config = builtins.readFile ./kanata.lisp;}
   ];
 
-  environment.systemPackages = with pkgs; [
-    (inputs.wrapper-manager.lib.build {
-      inherit pkgs;
-      modules = [../wrappers.nix];
-    })
-    gh
-    fd
-    git
-    dwl
-    fzf
-    gcc
-    pass
-    swww
-    tmux
-    comma
-    direnv
-    neovim
-    zoxide
-    ripgrep
-    undollar
-    tealdeer
-    oh-my-posh
-    qutebrowser
-    wl-clipboard
-    bibata-cursors
-  ] ++ [
-    st
-    dwl
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      (inputs.wrapper-manager.lib.build {
+        inherit pkgs;
+        modules = [../wrappers.nix];
+      })
+      gh
+      fd
+      git
+      dwl
+      fzf
+      gcc
+      pass
+      swww
+      tmux
+      comma
+      direnv
+      neovim
+      zoxide
+      ripgrep
+      undollar
+      tealdeer
+      oh-my-posh
+      qutebrowser
+      wl-clipboard
+      bibata-cursors
+    ]
+    ++ [
+      st
+      dwl
+      swhkd
+    ];
 
-  services.displayManager.sessionPackages = [ dwl ];
+  services.displayManager.sessionPackages = [dwl];
 
   fonts.packages = with pkgs; [
     (nerdfonts.override {fonts = ["JetBrainsMono"];})
