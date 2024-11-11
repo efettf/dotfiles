154cad834d29b56ee1850572851d22e907dafdf7
efettf <efettf@proton.me>
2024-11-11 21:29:48 +0100

    automatic networking hostname

--- default.nix
+++ default.nix
@@ -2,6 +2,6 @@ self:
 with self; let
   mkSystem = host: {
     specialArgs = {inherit inputs;};
-    modules = [./share ./hosts/${host} ./modules/homix.nix];
+    modules = [./share ./hosts/${host} ./modules/homix.nix {networking.hostName = host;}];
   };
 in {nixos = mkSystem "nixos";}
