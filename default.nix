5f10ed8192d5b89ebb4ff9a6807656d99dcbce45
efettf <efettf@proton.me>
2024-11-11 20:18:59 +0100

    remove dev shells

--- default.nix
+++ default.nix
@@ -1,12 +1,7 @@
 self:
 with self; let
-  mkSystem = host:
-    inputs.nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./share
-        ./hosts/${host}
-        ./modules/homix.nix
-      ];
-    };
+  mkSystem = host: {
+    specialArgs = {inherit inputs;};
+    modules = [./share ./hosts/${host} ./modules/homix.nix];
+  };
 in {nixos = mkSystem "nixos";}
