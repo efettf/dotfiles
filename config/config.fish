d627671bdc025eac646479ae3abbb3c6ffe9027c
efettf <efettf@proton.me>
2024-11-16 20:09:04 +0100

    fish vi mode

--- config/config.fish
+++ config/config.fish
@@ -1,6 +1,5 @@
 
-set EDITOR nvim
-set fish_greeting
+set EDITOR 'nvim'
 
 abbr n 'git'
 abbr g 'git'
@@ -8,6 +7,15 @@ abbr g 'git'
 alias zrm 'zoxide remove $(pwd)'
 alias v   'eval "$EDITOR"'
 alias l   'eza'
+
+fish_vi_cursor
+fish_vi_key_bindings
+
+set fish_greeting ''
+
+set fish_cursor_default 'underscore'
+set fish_cursor_insert  'underscore'
+set fish_cursor_visual  'underscore'
 
 zoxide init fish | source
 
