a26aa6d0f09107413cf89bbaa72765a2f2f724d0
efettf <efettf@proton.me>
2024-11-17 14:35:17 +0100

    add fundle

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,11 @@
 
+# Boostrap fundle.
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+
+fundle plugin 'zzhaolei/transient.fish'
+
+fundle init
+
 set EDITOR 'nvim'
 
 abbr n 'git'
