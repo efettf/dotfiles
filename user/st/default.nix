6d9fc0f896215505b74f0bd6778136bb1a7aadb1
efettf <efettf@proton.me>
2024-11-19 17:02:38 +0100

    change st cursor

--- user/st/default.nix
+++ user/st/default.nix
@@ -8,6 +8,7 @@
         })
         ./patches/font.diff
         ./patches/theme.diff
+        ./patches/cursor.diff
       ];
       buildInputs = old.buildInputs ++ [harfbuzz xorg.libXcursor];
     }))
