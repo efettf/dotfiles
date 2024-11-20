1a1d83c4d141ebec146ef97ad3042e5f2dedf3e2
efettf <efettf@proton.me>
2024-11-20 20:42:16 +0100

    fix antipatterns in nix

--- modules/files.nix
+++ modules/files.nix
@@ -6,85 +6,56 @@
 }:
 with builtins;
 with pkgs;
-with lib; {
-  options = {
-    files = mkOption {
-      default = {};
-      type = types.attrsOf (types.submodule ({
-        name,
-        config,
-        options,
-        ...
-      }: {
-        options = {
-          path = mkOption {
-            type = types.str;
-            description = ''
-              Path to the file relative to the $HOME directory.
-              If not defined, name of attribute set will be used.
-            '';
-          };
-          source = mkOption {
-            type = types.path;
-            description = "Path of the source file or directory.";
-          };
-          text = mkOption {
-            default = null;
-            type = types.nullOr types.lines;
-            description = "Text of the file.";
-          };
+with lib;
+with types; {
+  options.files = mkOption {
+    default = {};
+    type = attrsOf (submodule ({
+      name,
+      config,
+      options,
+      ...
+    }: {
+      options = {
+        path = mkOption {
+          type = str;
+          description = ''
+            Path to the file relative to the $HOME directory.
+            If not defined, name of attribute set will be used.
+          '';
         };
-        config = {
-          path = mkDefault name;
-          source = mkIf (config.text != null) (
-            let
-              name' = "files-" + replaceStrings ["/"] ["-"] name;
-            in
-              mkDerivedConfig options.text (writeText name')
-          );
+        source = mkOption {
+          type = path;
+          description = "Path of the source file or directory.";
         };
-      }));
-    };
-    users.users = mkOption {
-      type = types.attrsOf (types.submodule {
-        options.files = mkEnableOption "Enable homix for selected user";
-      });
-    };
+        text = mkOption {
+          default = null;
+          type = nullOr lines;
+          description = "Text of the file.";
+        };
+      };
+      config = {
+        path = mkDefault name;
+        source = mkIf (config.text != null) (
+          mkDerivedConfig options.text (writeText ("files-" + replaceStrings ["/"] ["-"] name))
+        );
+      };
+    }));
   };
 
-  config = let
-    # List of users managed by files.
-    users = attrNames (filterAttrs (name: user: user.files) config.users.users);
-
-    files-link = let
-      files = map (f: ''
-        FILE=$HOME/${f.path}
-        [[ -d ${f.source} ]] && rm $FILE
-        mkdir -p $(dirname $FILE)
-        diff $FILE ${f.source} || ln -sf ${f.source} $FILE
-      '') (attrValues config.files);
-    in
-      writeShellScript "files-link" ''
-        #!/bin/sh
-        ${concatStringsSep "\n" files}
+  config.systemd.services."files" = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "lynx";
+      ExecStart = writeShellScript "files-link" ''
+        ${concatStringsSep "\n" (map (f: ''
+          FILE=$HOME/${f.path}
+          [[ -d ${f.source} ]] && rm $FILE
+          mkdir -p $(dirname $FILE)
+          diff $FILE ${f.source} || ln -sf ${f.source} $FILE
+        '') (attrValues config.files))}
       '';
-
-    mkService = user: {
-      name = "files-" + user;
-      value = {
-        wantedBy = ["multi-user.target"];
-        description = "Setup files environment for ${user}.";
-        serviceConfig = {
-          Type = "oneshot";
-          User = user;
-          ExecStart = files-link;
-        };
-        environment.HOME = config.users.users.${user}.home;
-      };
     };
-
-    services = listToAttrs (map mkService users);
-  in {
-    systemd.services = services;
   };
 }
