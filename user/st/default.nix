198f9180cb0fe0a80fab83506deffd59167f8993
efettf <efettf@proton.me>
2024-11-21 20:41:43 +0100

    add with statement before st patches

--- user/st/default.nix
+++ user/st/default.nix
@@ -5,8 +5,8 @@
 }: {
   environment.systemPackages = with pkgs; [
     (st.overrideAttrs (old: {
-      patches = [
-        inputs.st-ligatures
+      patches = with inputs; [
+        st-ligatures
         ./patches/font.diff
         ./patches/theme.diff
         ./patches/cursor.diff
