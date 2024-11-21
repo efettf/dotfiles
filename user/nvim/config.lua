3b0923e1e688a0fb5c5ae04107a695e8d62703e8
efettf <efettf@proton.me>
2024-11-21 18:49:41 +0100

    move more nvim options to vim script

--- user/nvim/config.lua
+++ user/nvim/config.lua
@@ -1,39 +1,6 @@
 
 vim.cmd('source ~/.config/nvim/extra.vim')
 
-local o = vim.o
-local g = vim.g
-local p = vim.opt
-local a = vim.api
-local k = vim.keymap
-
-o.autoread = true
-o.backup = false
-o.clipboard = "unnamedplus"
-o.expandtab = true
-o.foldlevel = 6
-o.formatoptions = "cr"
-o.guicursor = "a:hor20"
-o.ignorecase = true
-o.incsearch = true
-o.linespace = 2
-o.magic = true
-o.laststatus = 0
-o.number = true
-o.showmode = false
-o.scrolloff = 8
-o.shiftwidth = 2
-o.smartcase = true
-o.signcolumn = "yes"
-o.smarttab = true
-o.ruler = false
-o.splitbelow = true
-o.splitright = true
-o.swapfile = false
-o.tabstop = 2
-o.termguicolors = true
-o.writebackup = false
-
 require("cmp").setup({
   sources = require("cmp").config.sources({
     { name = "path" },
@@ -50,13 +17,4 @@ require("cmp").setup({
     ['<tab>'] = require('cmp').mapping.confirm({ select = true })
   })
 })
-
-g.mapleader = " "
-
-k.set("n", "<leader>f", "<cmd>Telescope find_files<cr>")
-k.set("n", "<leader>n", "<cmd>Telescope grep_string<cr>")
-
-k.set("n", "-", "<cmd>Oil<cr>")
-
-p.shortmess:append "sI"
 
--- user/nvim/config.vim
+++ user/nvim/config.vim
@@ -1,5 +1,39 @@
 
-nno Z ZZ
+set autoread
+set nobackup
+set clipboard="unnamedplus"
+set expandtab
+set foldlevel=6
+set formatoptions="cr"
+set guicursor="a:hor20"
+set ignorecase
+set incsearch
+set linespace=2
+set magic
+set laststatus=0
+set number
+set noshowmode
+set scrolloff=8
+set shiftwidth=2
+set smartcase
+set smarttab
+set noruler
+set splitbelow
+set splitright
+set noswapfile
+set tabstop=2
+set termguicolors
+set nowritebackup
 
+
+let mapleader=" "
+
+
+nno <leader>f <cmd>Telescope find_files<cr>
+nno <leader>n <cmd>Telescope grep_string<cr>
+
+nno - <cmd>Oil<cr>
+
+nno Z ZZ
 nno ; :
 
