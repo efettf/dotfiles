eed9c03e3beb2dee66c0b72afd112f85c86279e4
efettf <efettf@proton.me>
2024-11-25 14:19:32 +0100

    automatic config generator function

--- user/default.nix
+++ user/default.nix
@@ -1,15 +1,54 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  generateConfig = {
+    name,
+    import ? false,
+    importPath ? ./${name},
+    package ? true,
+    packageName ? pkgs.${name},
+    path ? "${name}/${name}.${format}",
+    pathText ? false,
+    pathSource ? true,
+    pathTextValue ? "",
+    pathSourceValue ? ./${name}.${format},
+    format,
+  }: {
+    imports =
+      if import == true
+      then [importPath]
+      else [];
+    config = {
+      files.".config/${path}" =
+        if pathSource == true
+        then {source = pathSourceValue;}
+        else
+          (
+            if pathText == true
+            then {text = pathSourceValue;}
+            else ""
+          );
+      environment.systemPackages =
+        if package == true
+        then [packageName]
+        else [];
+    };
+  };
+in {
   imports = [
     ./dwl
     ./fish
     ./git
-    ./gitu
     ./kanata
     ./nvim
     ./qutebrowser
     ./st
     ./tmux
     ./tty
+    (
+      generateConfig {
+        name = "gitu";
+        format = "toml";
+      }
+    )
   ];
   config = {
     environment.systemPackages = [
--- user/gitu/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  environment.systemPackages = lib.singleton pkgs.gitu;
-
-  files.".config/gitu/config.toml".source = ./config.toml;
-}
