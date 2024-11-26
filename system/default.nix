2b3b1d21728cd9491a117c0ee2868a961f94e5e4
efettf <efettf@proton.me>
2024-11-26 14:38:34 +0100

    rewrite system

--- system/default.nix
+++ system/default.nix
@@ -1,36 +1,76 @@
 {
   settings,
+  scheme,
   pkgs,
   lib,
   ...
 }: {
-  imports =
-    [
-      ./hardware.nix
-      ./kanata
-      ./tty
-    ]
-    ++ map (file: lib.importTOML file)
-    (lib.fileset.toList
-      (lib.fileset.fileFilter (file: file.hasExt "toml") ./.));
+  imports = [
+    ./hardware.nix
+    ./kanata
+  ];
 
-  config = {
-    fonts.packages = [(pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];})];
+  services = {
+    blueman.enable = true;
+    openssh.enable = true;
+    libinput.enable = true; # Enable touchpad support.
 
-    networking.hostName = settings.host;
-
-    environment.systemPackages = [pkgs.doas-sudo-shim];
-
-    time.timeZone = "Europe/Warsaw";
-
-    users.users.${settings.user} = {
-      isNormalUser = true;
-      initialPassword = "nixos";
-      extraGroups = ["wheel"];
+    displayManager.ly = {
+      enable = true;
+      settings.vi_mode = true;
     };
-
-    nix.settings.experimental-features = ["nix-command" "flakes"];
-
-    system.stateVersion = "24.05";
   };
+
+  systemd.targets = {
+    # Disable sleep.
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false;
+  };
+
+  console.colors = map (name: lib.strings.removePrefix "#" scheme."${name}") (builtins.attrNames scheme);
+
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+  };
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    networkmanager.enable = true;
+    firewall.enable = true;
+  };
+
+  security = {
+    sudo.enable = false;
+    doas = {
+      enable = true;
+      wheelNeedsPassword = false;
+    };
+  };
+
+  fonts.packages = [(pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];})];
+
+  networking.hostName = settings.host;
+
+  environment.systemPackages = [pkgs.doas-sudo-shim];
+
+  time.timeZone = "Europe/Warsaw";
+
+  users.users.${settings.user} = {
+    isNormalUser = true;
+    initialPassword = "nixos";
+    extraGroups = ["wheel"];
+  };
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  system.stateVersion = "24.05";
 }
