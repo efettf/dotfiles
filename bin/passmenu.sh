04d36b236430091fbeb7608c21f4ca9ab7a1c5c6
efettf <efettf@proton.me>
2024-11-27 18:38:28 +0100

    adjust passmenu script

--- bin/passmenu.sh
+++ bin/passmenu.sh
@@ -4,11 +4,11 @@ set -eu
 
 cd $HOME/dotfiles/secrets
 
-name=$(sops -d secrets.yaml | sed -n 'p;n' | sed -e 's/: |//g' | dmenu)
+name=$(sops -d secrets.yaml | sed -n 'p;n' | sed -e 's/: |//g' | dmenu -c -l 10)
 
 line=$(sops -d secrets.yaml | grep -n "$name" | head -n 1 | cut -d: -f1)
 
-sops -d secrets.yaml | sed -n "$(expr $line + 1)p" | sed -e 's/^[ \t]*//'
+sops -d secrets.yaml | sed -n "$(expr $line + 1)p" | sed -e 's/^[ \t]*//' | wl-copy
 
-
+notify-send "Done" "Password copied to user clipboard" -u low
 
