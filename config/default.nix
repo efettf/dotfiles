2333b2df7cce1caaf7e6db4f12893862e45bb8e0
efettf <efettf@proton.me>
2024-11-27 10:30:35 +0100

    add mako

--- config/default.nix
+++ config/default.nix
@@ -1,6 +1,6 @@
 {
   pkgs,
-  scheme, 
+  scheme,
   ...
 }: {
   imports = [
@@ -26,9 +26,11 @@
     tmux
     swww
     just
+    mako
     comma
     neovim
     wlsunset
+    libnotify
     qutebrowser
     wl-clipboard
 
@@ -53,12 +55,40 @@
     ".gitconfig".source = ./git.ini;
   };
 
-  files.".config/tmux/tmux.conf".text = 
-  ''
-    set -g pane-active-border-style bg=default,fg="${scheme.base02}"
-    set -g pane-border-style fg="${scheme.base02}"
-  '' 
-  + builtins.readFile ./tmux.conf;
+  files.".config/tmux/tmux.conf".text =
+    ''
+      set -g pane-active-border-style bg=default,fg="${scheme.base02}"
+      set -g pane-border-style fg="${scheme.base02}"
+    ''
+    + builtins.readFile ./tmux.conf;
+
+  files.".config/mako/config".text = ''
+    sort=-time
+    layer=overlay
+    background-color=${scheme.base00}
+    text-color=${scheme.base05}
+    width=250
+    height=100
+    border-size=2
+    border-color=${scheme.base04}
+    border-radius=1
+    icons=0
+    max-icon-size=64
+    ignore-timeout=1
+    font=JetBrainsMono Nerd Font 10
+
+    [urgency=low]
+    border-color=${scheme.base06}
+    default-timeout=2000
+
+    [urgency=normal]
+    border-color=${scheme.base09}
+    default-timeout=5000
+
+    [urgency=high]
+    border-color=${scheme.base08}
+    default-timeout=20000
+  '';
 
   programs.nano.enable = false;
 }
