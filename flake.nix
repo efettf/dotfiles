354ee010edd653b996d0842f1599e3be7b7b8d6e
efettf <efettf@proton.me>
2024-11-28 16:03:54 +0100

    change inputs styling

--- flake.nix
+++ flake.nix
@@ -31,58 +31,56 @@
     };
   };
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs."nixpkgs".url = "github:nixos/nixpkgs/nixos-24.11";
 
-    system.url = "github:efettf/system";
-    system.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."qutebrowser".url = "github:efettf/qutebrowser";
+  inputs."qutebrowser".inputs."nixpkgs".follows = "nixpkgs";
 
-    secrets.url = "github:efettf/secrets";
-    secrets.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."secrets".url = "github:efettf/secrets";
+  inputs."secrets".inputs."nixpkgs".follows = "nixpkgs";
 
-    qutebrowser.url = "github:efettf/qutebrowser";
-    qutebrowser.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ncmpcpp".url = "github:efettf/ncmpcpp";
+  inputs."ncmpcpp".inputs."nixpkgs".follows = "nixpkgs";
 
-    tmux.url = "github:efettf/tmux";
-    tmux.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."system".url = "github:efettf/system";
+  inputs."system".inputs."nixpkgs".follows = "nixpkgs";
 
-    bin.url = "github:efettf/bin";
-    bin.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."colors".url = "github:efettf/colors";
+  inputs."colors".inputs."nixpkgs".follows = "nixpkgs";
 
-    mako.url = "github:efettf/mako";
-    mako.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nsxiv".url = "github:efettf/nsxiv";
+  inputs."nsxiv".inputs."nixpkgs".follows = "nixpkgs";
 
-    nvim.url = "github:efettf/nvim";
-    nvim.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dmenu".url = "github:efettf/dmenu";
+  inputs."dmenu".inputs."nixpkgs".follows = "nixpkgs";
 
-    ncmpcpp.url = "github:efettf/ncmpcpp";
-    ncmpcpp.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."files".url = "github:efettf/files";
+  inputs."files".inputs."nixpkgs".follows = "nixpkgs";
 
-    git.url = "github:efettf/git";
-    git.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."gitu".url = "github:efettf/gitu";
+  inputs."gitu".inputs."nixpkgs".follows = "nixpkgs";
 
-    gitu.url = "github:efettf/gitu";
-    gitu.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."mako".url = "github:efettf/mako";
+  inputs."mako".inputs."nixpkgs".follows = "nixpkgs";
 
-    fish.url = "github:efettf/fish";
-    fish.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."tmux".url = "github:efettf/tmux";
+  inputs."tmux".inputs."nixpkgs".follows = "nixpkgs";
 
-    files.url = "github:efettf/files";
-    files.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nvim".url = "github:efettf/nvim";
+  inputs."nvim".inputs."nixpkgs".follows = "nixpkgs";
 
-    colors.url = "github:efettf/colors";
-    colors.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fish".url = "github:efettf/fish";
+  inputs."fish".inputs."nixpkgs".follows = "nixpkgs";
 
-    nsxiv.url = "github:efettf/nsxiv";
-    nsxiv.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."git".url = "github:efettf/git";
+  inputs."git".inputs."nixpkgs".follows = "nixpkgs";
 
-    dmenu.url = "github:efettf/dmenu";
-    dmenu.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dwl".url = "github:efettf/dwl";
+  inputs."dwl".inputs."nixpkgs".follows = "nixpkgs";
 
-    dwl.url = "github:efettf/dwl";
-    dwl.inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bin".url = "github:efettf/bin";
+  inputs."bin".inputs."nixpkgs".follows = "nixpkgs";
 
-    st.url = "github:efettf/st";
-    st.inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs."st".url = "github:efettf/st";
+  inputs."st".inputs."nixpkgs".follows = "nixpkgs";
 }
